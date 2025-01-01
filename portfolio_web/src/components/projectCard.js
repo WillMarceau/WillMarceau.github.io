0da70b37dbc3445b95d8bbdd67a1a7655b5ccf41
Will Marceau <willfmarceau@gmail.com>
2024-12-31 19:02:27 -0800

    almost done with project card layout

--- portfolio_web/src/components/projectCard.js
+++ portfolio_web/src/components/projectCard.js
@@ -4,14 +4,20 @@ function ProjectCard({ onClick, name, desc, role }) {
 
     return (
         <button className="projButton" onClick={onClick}>
-            <div>
-                Pic
-            </div>
-            <p>{name}</p>
-            <p>{desc}</p>
-            <p>{role}</p>
-            <div>
-                <p>tags here</p>
+            <div className="projButtonLayout">
+                <div>
+                    <img src="/lets-cook.png" alt="logo" className="projPic"/>
+                </div>
+                <div className="projInfo">
+                    <h4 className="projName montserrat-bold">{name}</h4>
+                    <p className="montserrat-light">{desc}</p>
+                    <div className="techList">
+                        <p className="montserrat-light">React</p>
+                        <p className="montserrat-light">CSS</p>
+                        <p className="montserrat-light">Python</p>
+                        <p className="montserrat-light">Flask</p>
+                    </div>
+                </div>
             </div>
         </button>
     )
