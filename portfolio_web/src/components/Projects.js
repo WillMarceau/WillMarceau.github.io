0506b07d5b45df69c91c49a63be63e8030fa7885
Will Marceau <willfmarceau@gmail.com>
2025-03-30 16:44:12 -0700

    fixed up project cards

--- portfolio_web/src/components/Projects.js
+++ portfolio_web/src/components/Projects.js
@@ -11,12 +11,11 @@ function Projects() {
                 with meal planning. The main purpose of the website is the user will be presented potential meals to cook in a Tinder-inspired manor. The user then can put the meal in their meal plan, dislikes or save the
                 meal to their liked meal list. Any meal added to the meal plan will automaticalluy populate a shopping list for you to make the process of meal planning as painless as possible." 
                 tags={["React", "CSS", "Python", "Flask", "SQL"]} link="https://github.com/lilylogan/cs422_project" img="/lets-cook.png"/>
-                <ProjectCardContainer name="MRI Tumor Classifier" desc="Currently in Progress, we are going to create two ML models that can scan MRI images to classify brain tumors into 
-                four categories, Glioma Tumor, Meningioma Tumor, Pituitary Tumor and No Tumor. We are attempting to use a Multilayer Perceptron Architecture and CNN, focusing both models on having 
-                a low Recall or false negative rate. We are then going to compare their effectiveness on our reletively small dataset"
-                tags={["Python", "Pytorch"]} link="https://github.com/WillMarceau/cs453_project" img="/MRI_Icon.png"/>
-                <ProjectCardContainer name="Story Secure" desc="Currently in Progress in a private Repo, this application aims to help those who can not keep up with Cyber Security Standards of today either due to memory issues, technical illiteracy or
-                an inability to access modern tech. Our solution is a local secure password manager where you sign in with a sequence of 6 images that form a story. This story password was inspired by memory research to enable users to better remember
+                <ProjectCardContainer name="MRI Tumor Classifier" desc="A CNN ML model with a focus on low recall rate that can scan MRI images to classify brain tumors into 
+                four categories, Glioma Tumor, Meningioma Tumor, Pituitary Tumor and No Tumor. It's best performance achieved over a 97% accuracy and 95% recall on all classes, Minimizing false negative diagnoses that could negatively impact a patient's survival rate."
+                tags={["Python", "Pytorch", "Juypter"]} link="https://github.com/WillMarceau/cs453_project" img="/MRI_Icon.png"/>
+                <ProjectCardContainer name="Story-Pass" desc="An application aims to help those who can not keep up with Cyber Security Standards of today either due to memory issues, technical illiteracy or
+                an inability to access modern tech. Our solution is a local secure password manager where you sign in with a sequence of 6 images that form a story. This story password was inspired by memory and graphical password research to enable users to better remember
                 a password, while keeping it hard to crack. This way we can give users a safe way to store more advanced passwords for other applications and keep up with evolving Security Standards." 
                 tags={["Python", "Tkinter", "SQLite"]} img="/StorySecure_Logo.webp"/>
             </div>
